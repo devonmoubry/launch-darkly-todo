d4f2a03d0e940aa2031b1723a35847db2ba48f89
Devon Moubry <devon@moubry.com>
2024-03-04 22:59:19 -0600

    adds conditional blue theme

--- src/todo/app.jsx
+++ src/todo/app.jsx
@@ -1,3 +1,4 @@
+import classNames from "classnames";
 import { useReducer } from "react";
 import { Header } from "./components/header";
 import { Main } from "./components/main";
@@ -6,15 +7,16 @@ import { Footer } from "./components/footer";
 import { todoReducer } from "./reducer";
 
 import "./app.css";
+import "./theme.css";
 
 export function App() {
     const [todos, dispatch] = useReducer(todoReducer, []);
 
     return (
-        <>
+        <div className={classNames({ 'new-theme': true })}>
             <Header dispatch={dispatch} />
             <Main todos={todos} dispatch={dispatch} />
             <Footer todos={todos} dispatch={dispatch} />
-        </>
+        </div>
     );
 }
